86da3de41330a9242be02824671c3edff085a744
AlexVitko <vpa24082012@tut.by>
2023-02-16 23:33:43 +0300

    Task4

--- Program.cs
+++ Program.cs
@@ -4,22 +4,37 @@
 // a=2;  b=10
 // a=-9; b=-3
 
+//Console.WriteLine("Ведите число a");
+//int numbera=Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Ведите число b");
+//int numberb=Convert.ToInt32(Console.ReadLine());
+
+//if (numbera >numberb)
+//{
+//Console.WriteLine("a=max");
+//Console.WriteLine("b=min");
+//}
+//else
+//{
+//Console.WriteLine("a=min");
+//Console.WriteLine("b=max");
+//}
+
+//Задача 4. Напишите программу, которая на вход принимает три числа, 
+// а на выход выдаетм.аксимальное их них
+//2, 3, 7
+//44, 5, 78
+//22, 3, 9 
+
 Console.WriteLine("Ведите число a");
 int numbera=Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Ведите число b");
 int numberb=Convert.ToInt32(Console.ReadLine());
 
-if (numbera >numberb)
-{
-Console.WriteLine("a=max");
-Console.WriteLine("b=min");
-}
-else
-{
- Console.WriteLine("a=min");
- Console.WriteLine("b=max");
-}
+Console.WriteLine("Ведите число c");
+int numberc=Convert.ToInt32(Console.ReadLine());
 
 
 
