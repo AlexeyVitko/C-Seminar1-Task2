b9025dbcaf9f77724cf11a4f62d543affb08fae5
AlexVitko <vpa24082012@tut.by>
2023-02-16 23:05:47 +0300

    Task2

--- Program.cs
+++ Program.cs
@@ -1,28 +1,25 @@
-﻿// Задача 2. Напишите программу, которая на вход принимает два числа, 
+﻿//Задача 2. Напишите программу, которая на вход принимает два числа, 
 // а на выход выдает, какое из чисел большее, а какое меньшее.
 // a=5;  b=7
 // a=2;  b=10
 // a=-9; b=-3
-//
-//Console.WriteLine("Ведите число a");
-//int numbera=Convert.ToInt32(Console.ReadLine());
-//
-//Console.WriteLine("Ведите число b");
-//int numberb=Convert.ToInt32(Console.ReadLine());
-//
-//if (numbera >numberb)
-//{
-  //Console.WriteLine("a=max");
-  //Console.WriteLine("b=min");
-//}
-//else
-//{
-// Console.WriteLine("a=min");
-// Console.WriteLine("b=max");
-//}
 
-// Задача 4. Напишите программу, которая на вход принимает три числа 
-//и выдает максимальное из трех чисел.
-//2 3 7
-//44 5 78
-//22 3 9
+Console.WriteLine("Ведите число a");
+int numbera=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите число b");
+int numberb=Convert.ToInt32(Console.ReadLine());
+
+if (numbera >numberb)
+{
+Console.WriteLine("a=max");
+Console.WriteLine("b=min");
+}
+else
+{
+ Console.WriteLine("a=min");
+ Console.WriteLine("b=max");
+}
+
+
+
